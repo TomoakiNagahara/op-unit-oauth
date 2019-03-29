4c738f2db4707605b50e57ffd4f19eff26418a20
Tomoaki imac5k-1 <tomoaki.nagahara@gmail.com>
2019-03-29 15:53:22 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-oauth:/index.php
+ *
+ * @creation  2018-07-03
+ * @version   1.0
+ * @package   unit-oauth
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include_once('OAuth.class.php');
